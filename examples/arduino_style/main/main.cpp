934ab4f89b8212f5e42c8e1f7f55e0aad7ec6372
Ibe Ibrahim <ibraheli41@gmail.com>
2025-09-10 06:51:50 +0700

    Update main.cpp

--- examples/arduino_style/main/main.cpp
+++ examples/arduino_style/main/main.cpp
@@ -1,26 +1,34 @@
 //file: main.cpp
-
 #include "Arduino.h"
-
-#ifdef ARDUINO_ARCH_ESP32
-#include "esp32-hal-log.h"
-#endif
+#include "esp_log.h"
 
 const int ledPin = 2;
+static const char* pMY_TAG = "LED";
 
 void setup(){
   Serial.begin(115200);
   while(!Serial){
     ; // wait for serial port to connect
   }
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);  // Initialize Pin as Output
 }
 
 void loop(){
-    digitalWrite(ledPin, HIGH);
-    Serial.println("LED HIGH");
-    delay(1000);
-    digitalWrite(ledPin, LOW);
-    Serial.println("LED LOW");
-    delay(1000);
+  // Turn on LED
+  digitalWrite(ledPin, HIGH);
+  
+  // Added LOG INFO
+  ESP_LOGI(pMY_TAG, "ON");    // ESP-IDF LOG
+  Serial.println("LED HIGH"); // Arduino Style
+  
+  delay(1000);
+  
+  // Turn off LED
+  digitalWrite(ledPin, LOW);
+  
+  // Added LOG INFO
+  ESP_LOGI(pMY_TAG, "OFF");   // ESP-IDF LOG
+  Serial.println("LED LOW");  // Arduino Style
+    
+  delay(1000);
 }
