6e4b12215effeb2317f7ba8ffb7d84462dbe931c
bokumentation <ibraheli41@gmail.com>
2025-09-13 02:15:41 +0700

    update

--- examples/extern_c_style/main/main.cpp
+++ examples/extern_c_style/main/main.cpp
@@ -1,31 +1,21 @@
 // file: main.cpp
 #include "Arduino.h"
 
-const int ledPin = 2;
-static const char *pMY_TAG = "LED";
+extern "C" void app_main() {
+  initArduino(); // Initialize the Arduino core
 
-void setup() {
+  // Arduino-like setup()
   Serial.begin(115200);
   while (!Serial) {
-    ; // wait for serial port to connect
+    ; // Wait for serial port to connect
   }
 
-  pinMode(ledPin, OUTPUT); // Initialize Pin as Output
+  // Arduino-like loop()
+  while (true) {
+    Serial.println("Looping...");
+    delay(1000); // Wait for 1 second
+  }
 
-  ESP_LOGI("TAG", "HELLO from ESP_LOG Info!");        // ESP-IDF Log Info
-  Serial.println("HELLO from ARDUINO Serial Print!"); // Arduino Serial Print
-
-  delay(3000);
-}
-
-void loop() {
-  digitalWrite(ledPin, HIGH); // Turn on LED
-  ESP_LOGI(pMY_TAG, "ON");
-  Serial.println("LED HIGH");
-  delay(1000);
-
-  digitalWrite(ledPin, LOW); // Turn off LED
-  ESP_LOGI(pMY_TAG, "OFF");
-  Serial.println("LED LOW");
-  delay(1000);
+  // WARNING: If the program reaches the end of app_main(), the MCU will
+  // restart.
 }
