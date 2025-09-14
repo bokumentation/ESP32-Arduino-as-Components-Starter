6aaf0b3acddb71455fa7698900537266deb94580
Ibe Ibrahim <ibraheli41@gmail.com>
2025-09-14 13:05:02 +0700

    added

---
+++ examples/esp32c3_arduino/main/main.cpp
@@ -0,0 +1,31 @@
+// file: main.cpp
+#include "Arduino.h"
+
+const int ledPin = 8;
+static const char *pMY_TAG = "LED";
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect
+  }
+
+  pinMode(ledPin, OUTPUT); // Initialize Pin as Output
+
+  ESP_LOGI("TAG", "HELLO from ESP_LOG Info!");        // ESP-IDF Log Info
+  Serial.println("HELLO from ARDUINO Serial Print!"); // Arduino Serial Print
+
+  delay(3000);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // Turn on LED
+  ESP_LOGI(pMY_TAG, "ON");
+  Serial.println("LED HIGH");
+  delay(1000);
+
+  digitalWrite(ledPin, LOW); // Turn off LED
+  ESP_LOGI(pMY_TAG, "OFF");
+  Serial.println("LED LOW");
+  delay(1000);
+}
